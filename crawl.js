0ce195301785cbe5b650fe8eaca53ca0043f5623
Aalishan17Raza <aalishan69raza@gmail.com>
2023-08-23 02:53:39 +0530

    added new tests

--- crawl.js
+++ crawl.js
@@ -1,5 +1,38 @@
-function NormalizeUrl(urlString) {
-    return urlString;
+const { JSDOM } = require("jsdom");
+function getUrlsFromHTML(HTMLbody, baseUrl) {
+    const urls = [];
+    const dom = new JSDOM(HTMLbody);
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
+            //relative url
+            try {
+                const urlObj = new URL(`${baseUrl}${linkElement.href}`);
+                urls.push(urlObj.href);
+            } catch (error) {
+                console.log("invalid url");
+            }
+
+        }else{
+            //absolute url
+            try {
+                const urlObj = new URL(linkElement.href);
+                urls.push(urlObj.href);
+            } catch (error) {
+                console.log("invalid url");
+            }
+        }
+    }
+    return urls;
 }
 
-module.exports = { NormalizeUrl }
+function NormalizeUrl(url) {
+    const urlObj = new URL(url)
+    let fullPath = `${urlObj.host}${urlObj.pathname}`;
+    if (fullPath.length > 0 && fullPath.slice(-1) === '/') {
+        fullPath = fullPath.slice(0, -1)
+    }
+    return fullPath.toLowerCase();
+}
+
+module.exports = { NormalizeUrl, getUrlsFromHTML };
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,70 @@
-const { NormalizeUrl } = require("./crawl.js");
+const { NormalizeUrl,getUrlsFromHTML } = require("./crawl.js");
 
 test('NormalizeUrl', () => {
-    expect(NormalizeUrl("hello")).toBe("hell");
+    expect(NormalizeUrl("http://blog.boot.dev/path/")).toBe("blog.boot.dev/path");
+});
+
+test('NormalizeUrl', () => {
+    expect(NormalizeUrl("https://Blog.boot.dev/path/")).toBe("blog.boot.dev/path");
+});
+
+test('NormalizeUrl', () => {
+    expect(NormalizeUrl("https://Blog.boot.dev/path")).toBe("blog.boot.dev/path");
+});
+
+test('NormalizeUrl', () => {
+    expect(NormalizeUrl("https://Blog.boot.dev/path?q=tgggv")).toBe("blog.boot.dev/path");
+});
+
+test('getUrlsFromHTML absolute', () => {
+    const inputHTMLbody =`
+    <html>
+        <body>
+            <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+        </body>
+    </html>
+    `;
+    const inputbaseURL='https://blog.boot.dev';
+    expect(getUrlsFromHTML(inputHTMLbody,inputbaseURL)).toEqual(["https://blog.boot.dev/"]);
+});
+
+test('getUrlsFromHTML relative', () => {
+    const inputHTMLbody =`
+    <html>
+        <body>
+            <a href="/path/"><span>Go to Boot.dev</span></a>
+        </body>
+    </html>
+    `;
+    const inputbaseURL='https://blog.boot.dev';
+    expect(getUrlsFromHTML(inputHTMLbody,inputbaseURL))
+    .toEqual(["https://blog.boot.dev/path/"]);
+});
+
+test('getUrlsFromHTML both', () => {
+    const inputHTMLbody =`
+    <html>
+        <body>
+            <a href="/path2/"><span>Go to Boot.dev</span></a>
+            <a href="https://blog.boot.dev/path1/"><span>Go to Boot.dev</span></a>
+        </body>
+    </html>
+    `;
+    const inputbaseURL='https://blog.boot.dev';
+    expect(getUrlsFromHTML(inputHTMLbody,inputbaseURL))
+    .toEqual(["https://blog.boot.dev/path2/","https://blog.boot.dev/path1/"]);
+});
+
+test('getUrlsFromHTML invalid', () => {
+    const inputHTMLbody =`
+    <html>
+        <body>
+            <a href="invalid"><span>Go to Boot.dev</span></a>
+            <a href="https://blog.boot.dev/path1/"><span>Go to Boot.dev</span></a>
+        </body>
+    </html>
+    `;
+    const inputbaseURL='https://blog.boot.dev';
+    expect(getUrlsFromHTML(inputHTMLbody,inputbaseURL))
+    .toEqual(["https://blog.boot.dev/path1/"]);
 });
